75974a2ecd95bb322a8a826d765d8d6b8d47b2be
josejhonata <josetrevisan04@gmail.com>
2021-01-22 23:42:15 -0300

    Criando models de registro

---
+++ seguros/app/Http/Controllers/RegistroController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\registro;
+use Illuminate\Http\Request;
+
+class RegistroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\registro  $registro
+     * @return \Illuminate\Http\Response
+     */
+    public function show(registro $registro)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\registro  $registro
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(registro $registro)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\registro  $registro
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, registro $registro)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\registro  $registro
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(registro $registro)
+    {
+        //
+    }
+}
---
+++ seguros/database/migrations/2021_01_23_023517_create_registros_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registros', function (Blueprint $table) {
+            $table->id();
+            $table->string('local_do_acidente',40);
+            $table->string('data_do_acidente',20);
+            $table->integer('valor_do_prejuizo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registros');
+    }
+}
