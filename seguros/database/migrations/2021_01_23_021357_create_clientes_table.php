a3f17d5af4d7c8e0a6ceb094a67a2d4764703616
josejhonata <josetrevisan04@gmail.com>
2021-01-22 23:30:00 -0300

    Criando models de cliente

---
+++ seguros/database/migrations/2021_01_23_021357_create_clientes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome',45);
+            $table->string('cpf',14);
+            $table->string('senha',32);
+            $table->integer('numero_da_carteira');
+            $table->string('endereço',45);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
