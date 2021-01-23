a43fd6f35e859307c8927ca31b6b7bb10ad38925
josejhonata <josetrevisan04@gmail.com>
2021-01-22 23:34:31 -0300

    Criando models da tabela carros

---
+++ seguros/app/Http/Controllers/CarroController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\carro;
+use Illuminate\Http\Request;
+
+class CarroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\carro  $carro
+     * @return \Illuminate\Http\Response
+     */
+    public function show(carro $carro)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\carro  $carro
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(carro $carro)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\carro  $carro
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, carro $carro)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\carro  $carro
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(carro $carro)
+    {
+        //
+    }
+}
---
+++ seguros/database/migrations/2021_01_23_023206_create_carros_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carros', function (Blueprint $table) {
+            $table->id();
+            $table->string('placa',15);
+            $table->string('cor',10);
+            $table->string('modelo',20);
+            $table->integer('ano');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carros');
+    }
+}
